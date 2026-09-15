16e400b312f0de38e236251806226140d4a3b280
Marco <46133335+Marco4413@users.noreply.github.com>
2026-09-15 14:20:01 +0200

    example: added serializing_with_options

---
+++ examples/serializing_with_options.cpp
@@ -0,0 +1,29 @@
+#define JPP_IMPLEMENTATION
+#include <jpp/jpp.h>
+
+#include <iostream>
+
+#include "common/args.cpp"
+#include "common/files.cpp"
+
+int main(int argc, const char **argv)
+{
+    shift_args(argc, argv, nullptr); // program_name
+    const std::filesystem::path file_path = shift_args(argc, argv, "examples/serializing_with_options.json");
+
+    const std::string json_content = read_file(file_path);
+
+    jpp::parser::error error;
+    jpp::value value = jpp::parse(json_content, &error);
+    if (error.is_set()) {
+        std::cerr << error.to_string(file_path.string()) << std::endl;
+        return 1;
+    }
+
+    jpp::serializer::options options;
+    options.indent_size = 0;
+
+    std::cout << jpp::serialize(value, options) << std::endl;
+
+    return 0;
+}
